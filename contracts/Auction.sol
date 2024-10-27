b739af2a427867ec77edcc0a8b3bdc72b24db624
TenerSed <74030082+TenerSed@users.noreply.github.com>
2024-10-27 13:56:38 -0400

    Finished Assignment

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -6,20 +6,50 @@ contract Auction {
     uint256 public minimumBid ;
     address public maxBidder ;
     bool public auctionEnded ;
+    uint256 public highestBid ;
+    bool public anyBids ;
+
 
     // Constructor
     constructor ( uint256 _minimumBid , address payable _beneficiaryAddress ) {
-        // TODO : Initialize the contract
+        minimumBid = _minimumBid;
+        beneficiary = _beneficiaryAddress;
+        auctionEnded = false;
+        highestBid = 0;
+        anyBids = false;
     }
 
     // Function to place a bid
     function bid () external payable {
-        // TODO : Implement the bid function
+        require(!auctionEnded, "Auction has ended.");
+        require(msg.sender != beneficiary, "Beneficiary cannot bid.");
+        require(msg.sender != maxBidder, "You are already the highest bidder.");
+        require(msg.value >= minimumBid, "Bid is below the minimum bid.");
+
+        // Refund the previous highest bidder
+        if (minimumBid != 0) {
+            payable(maxBidder).transfer(highestBid);
+        }
+
+        maxBidder = msg.sender;
+        highestBid = msg.value;
+        minimumBid = msg.value;
     }
 
     // Function to end the auction
     function settleAuction () external {
-        // TODO : Implement the settleAuction function
-    }
+        require(!auctionEnded, "Auction has already been settled.");
+        require(msg.sender == beneficiary, "Only the beneficiary can end the auction.");
+
+        if (!anyBids) {
+            maxBidder = beneficiary;
+        }
+
+        // End the auction and transfer funds to the beneficiary
+        auctionEnded = true;
+        if (highestBid > 0) {
+            beneficiary.transfer(highestBid);
+        }
+    }   
 
 }
